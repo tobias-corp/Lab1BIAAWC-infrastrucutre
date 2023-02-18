edd6689566133c6f17cd45c77ea6196ce323cfd6
Tobi <75902541+Tobiasz-Hurny@users.noreply.github.com>
2023-02-18 14:33:57 +0100

    create veriables.tf

---
+++ terraform/veriables.tf
@@ -0,0 +1,7 @@
+variable "key_pair_name" {
+  description = "The name of the key pair to use for SSH access"
+}
+
+variable "key_pair_path" {
+  description = "The local path to the private key for the specified key pair"
+}
