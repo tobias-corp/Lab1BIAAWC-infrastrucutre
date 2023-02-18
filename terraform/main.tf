f24cd10e6b349ebfd570d147743ac1685b025369
Tobi <75902541+Tobiasz-Hurny@users.noreply.github.com>
2023-02-18 14:46:25 +0100

    update main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.75.0"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
 provider "aws" {
   region = "us-west-2"
 }
