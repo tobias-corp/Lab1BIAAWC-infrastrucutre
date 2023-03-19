6132077171d865c25d7ef0f4024de5b662efa1c8
Tobi <75902541+Tobiasz-Hurny@users.noreply.github.com>
2023-03-19 16:44:38 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,43 +1,67 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.75.0"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
-
+# Define the provider for AWS
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+# Create a new security group for the app
+resource "aws_security_group" "flask_app_sg" {
+  name_prefix = "flask_app_sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create a new EC2 instance to run the app
+resource "aws_instance" "flask_app" {
+  ami           = "ami-0c94855ba95c71c99"
   instance_type = "t2.micro"
-  key_name      = var.key_pair_name
+  key_name      = "my_key_pair"
+  security_groups = [
+    aws_security_group.flask_app_sg.id,
+  ]
+
+  # Use user_data to install Python and Flask, and start the app
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum update -y
+              sudo yum install -y python3-pip
+              sudo pip3 install flask
+              sudo echo '
+              import os
+              from flask import Flask, send_from_directory
+              
+              app = Flask(__name__)
+
+              @app.route("/")
+              def index():
+                  return "Hello, World!"
+
+              @app.route("/greet/<name>")
+              def greet(name):
+                  return f"Hello, {name}!"
+
+              @app.route("/static/<path:path>")
+              def serve_static(path):
+                  root_dir = os.getcwd()
+                  return send_from_directory(os.path.join(root_dir, "static"), path)
+
+              if __name__ == "__main__":
+                  app.run(host="0.0.0.0")
+              ' > /home/ec2-user/app.py
+              sudo chmod +x /home/ec2-user/app.py
+              sudo nohup python3 /home/ec2-user/app.py > /dev/null 2>&1 &
+              EOF
 
   tags = {
-    Name = "Hello World EC2 Instance"
+    Name = "flask-app-instance"
   }
+}
 
-  provisioner "file" {
-    source      = "app.py"
-    destination = "/home/ubuntu/app.py"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y python3",
-      "sudo python3 /home/ubuntu/app.py &"
-    ]
-  }
-
-  connection {
-    type        = "ssh"
-    user        = "ubuntu"
-    private_key = file(var.key_pair_path)
-    host        = self.public_ip
-  }
+# Output the public IP address of the instance
+output "public_ip" {
+  value = aws_instance.flask_app.public_ip
 }
